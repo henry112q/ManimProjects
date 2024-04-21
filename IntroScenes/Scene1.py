4ddef107572001b915d0db370415772ddb8c1ee1
henry112q <138253381+henry112q@users.noreply.github.com>
2024-04-21 16:00:42 +0100

    introScene1

--- IntroScenes/Scene1.py
+++ IntroScenes/Scene1.py
@@ -3,4 +3,17 @@ from manim import *
 
 class IntroScene(Scene):
     def construct(self):
-        pass
+        square = Square()
+        circle = Circle()
+        triangle = Triangle()
+        square.set_fill(YELLOW,opacity=0.5)
+        circle.set_fill(ORANGE,opacity=0.7)
+        triangle.set_fill(RED,opacity=0.9)
+        square.move_to(LEFT/2)
+        triangle.move_to(RIGHT/2)
+        self.play(FadeIn(square))
+        self.play(square.animate.shift(RIGHT/2),ReplacementTransform(square,circle),run_time=2)
+        self.wait(0.5)
+        self.play(circle.animate.shift(RIGHT/2),ReplacementTransform(circle,triangle), run_time=2)
+        self.wait(0.5)
+        self.play(FadeOut(triangle))
